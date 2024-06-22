8ede947b0a50f11badd4036504820fc56e511a78
Aakanksha83 <visamshettyaakanksha@gmail.com>
2024-06-22 15:30:54 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.*; 
+class Sum{
+public static void main(String args[]){
+int n,t=0;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+for(int i=0;i<=n;i++){
+t=t+i;
+}
+System.out.println(t);
+}
+}
