a4a42012fea529c11b47b9d47b24eb21c70a5ee9
Aakanksha83 <visamshettyaakanksha@gmail.com>
2024-06-22 16:08:19 +0530

    Add files via upload

---
+++ Prime1.java
@@ -0,0 +1,29 @@
+import java.util.*; 
+class Prime1{
+public static void main(String args[]){
+int n,i,c=0,r=0;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+if(n==1){
+c=1;
+}
+else{
+for(i=1;i<=n/2;i++){
+r++;
+if(n%i==0)
+{
+c++;
+break;
+}
+}
+}
+if(c==2){
+System.out.println("Prime no");
+}
+else{
+System.out.println("not a prime");
+}
+}
+}
+
+
