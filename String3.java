b52327a6662edd7279c7e1d7ae1c0b3e4edc04af
Aakanksha83 <visamshettyaakanksha@gmail.com>
2024-07-02 13:55:55 +0530

    Add files via upload

---
+++ String3.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class String3 {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        String str="java is simple LANGUAGE";
+        System.out.println(str.toLowerCase());
+         System.out.println(str.toUpperCase());
+    }
+}
