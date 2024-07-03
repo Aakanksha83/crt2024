19f18d2399cea466e355663207303a7399fe9774
Aakanksha83 <visamshettyaakanksha@gmail.com>
2024-07-03 13:46:36 +0530

    Add files via upload

---
+++ StringInt.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class StringInt {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+    
+        System.out.println(60+40);
+        System.out.println("wel"+"come");
+        System.out.println(60+"python");
+        
+    }
+}
