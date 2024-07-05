80949345931bd02fc2fda3ce92acd49f768f5331
Aakanksha83 <visamshettyaakanksha@gmail.com>
2024-07-05 13:43:54 +0530

    Add files via upload

---
+++ MoveZero.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class MoveZero{
+    public static void main(String[] args) {
+      Scanner s=new Scanner(System.in);
+      int n=s.nextInt();
+      int a[]=new int[n];
+      for(int i=0;i<n;i++){
+          a[i]=s.nextInt();
+      }
+      for(int i=0;i<n;i++){
+          if(a[i]!=0)
+         
+        System.out.print(a[i]+" ");
+    }
+    for(int i=0;i<n;i++){
+        if(a[i]==0)
+         System.out.print(a[i]+" ");
+}
+}
+}
